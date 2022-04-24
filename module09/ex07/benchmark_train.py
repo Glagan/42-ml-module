1eddd3ce8e3022919de513bb87ff232a4916cca5
Glagan <8447621+Glagan@users.noreply.github.com>
2022-04-24 15:57:45 +0200

    Add models for different lambdas

--- module09/ex07/benchmark_train.py
+++ module09/ex07/benchmark_train.py
@@ -1,3 +1,4 @@
+import math
 from turtle import fd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -25,51 +26,53 @@ thetas = [
     np.random.rand(7, 1),
     np.random.rand(10, 1),
     np.random.rand(13, 1),
-    # Train more polynomial for testing only -- they are not saved
-    np.random.rand(16, 1),
-    np.random.rand(19, 1),
 ]
 max_polynomial = len(thetas)
 
 # Train models
 alpha = 0.005
-max_iter = 100000
+max_iter = 50000
 models = []
 models_loss = []
 losses = []
+lambdas = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
 for i in range(0, max_polynomial):
-    print(f"[Polynomial {i + 1}] Training...")
-    model = MyRidge(thetas[i] + 0, alpha=alpha, max_iter=max_iter)
-    # Normalize dataset after polynomials
-    x = add_polynomial_features(xTrain, i + 1)
-    # print("thetas", thetas[i].shape)
-    # print("x", x.shape)
-    norm_mean, norm_std = x.mean(axis=0), x.std(axis=0)
-    x = (x - norm_mean) / norm_std
-    fdfdfd = model.fit_(x, yTrain, check_loss=True)
-    _, loss = fdfdfd
-    models_loss.append(loss)
-    # Calculate loss against test set
-    # Test set is normalized using the same values as the training set
-    x_test = add_polynomial_features(xTest, i + 1)
-    x_test = (x_test - norm_mean) / norm_std
-    loss = model.loss_(yTest, model.predict_(x_test))
-    print(f"[Polynomial {i + 1}] Test loss: {loss}")
-    losses.append(loss)
-    models.append(model)
+    for lambda_ in lambdas:
+        print(f"[Polynomial {i + 1} / λ {lambda_}] Training...")
+        model = MyRidge(thetas[i] + 0, alpha=alpha, max_iter=max_iter, lambda_=lambda_)
+        # Normalize dataset after polynomials
+        x = add_polynomial_features(xTrain, i + 1)
+        norm_mean, norm_std = x.mean(axis=0), x.std(axis=0)
+        x = (x - norm_mean) / norm_std
+        _, loss = model.fit_(x, yTrain, check_loss=True)
+        models_loss.append(loss)
+        # Calculate loss against test set
+        # Test set is normalized using the same values as the training set
+        x_test = add_polynomial_features(xTest, i + 1)
+        x_test = (x_test - norm_mean) / norm_std
+        loss = model.loss_(yTest, model.predict_(x_test))
+        print(f"[Polynomial {i + 1} / λ {lambda_}] Test loss: {loss}")
+        losses.append(loss)
+        models.append(model)
 
 # Save trained models
 # -- with the alpha and iterations
 # -- and the initial thetas for the best model
 np.savez("models.npz",
-         np.array([alpha, max_iter, 4]),
-         thetas[3],  # The fourth model is the best
-         *[model.thetas for model in models[:4]])
+         # A lambda_ value of 0.6 is good without increasing loss too much
+         np.array([alpha, max_iter, 4, 0.6]),
+         # The model with 4 polynomial is the best
+         thetas[3],
+         *[model.thetas for model in models])
 
 # Show loss over time for the trained models on the test set
-cmap = plt.cm.get_cmap('jet', max_polynomial + 1)
+cmap = plt.cm.get_cmap('jet', len(models) + 1)
+j = 0
 for index, model_loss in enumerate(models_loss):
-    plt.plot(list(range(1, max_iter + 1)), model_loss, c=cmap(index + 1), label=f"Polynomial {index + 1}")
+    polynomial = math.floor(index / 6) + 1
+    lambda_ = lambdas[j % len(lambdas)]
+    plt.plot(list(range(1, max_iter + 1)), model_loss, c=cmap(index + 1), label=f"Polynomial {polynomial} / λ {lambda_}")
+    j += 1
 plt.xlabel("Epoch")
 plt.ylabel("Loss")
 plt.legend()
--- module09/ex07/space_avocado.py
+++ module09/ex07/space_avocado.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -14,21 +15,17 @@ except BaseException as error:
 features = ['weight', 'prod_distance', 'time_delivery']
 x = df[features].to_numpy().reshape(-1, 3)
 y = df['target'].to_numpy().reshape(-1, 1)
+lambdas = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
 
 # Load trained models and the best model initial thetas
 try:
     models = np.load('models.npz')
-    if len(models) != 6:
+    if len(models) != 26:
         raise ValueError("missing value in saved models")
-    alpha, max_iter, polynomials = [value.item() for value in models["arr_0"]]
-    print(alpha, max_iter, polynomials)
+    alpha, max_iter, polynomials, lambda_ = [value.item() for value in models["arr_0"]]
+    print(alpha, max_iter, polynomials, lambda_)
     best_model_theta = models["arr_1"]
-    other_models = [
-        models["arr_2"],
-        models["arr_3"],
-        models["arr_4"],
-        models["arr_5"]
-    ]
+    other_models = [models[f"arr_{index}"] for index in range(2, 26)]
 except BaseException as error:
     print(f"Failed to read dataset: {error}")
     exit(1)
@@ -41,7 +38,7 @@ x_pol = add_polynomial_features(xTrain, int(polynomials))
 norm_mean, norm_std = x_pol.mean(axis=0), x_pol.std(axis=0)
 x_pol = (x_pol - norm_mean) / norm_std
 # Train model
-model = MyRidge(best_model_theta, alpha=alpha, max_iter=int(max_iter))
+model = MyRidge(best_model_theta, alpha=alpha, max_iter=int(max_iter), lambda_=lambda_)
 print("Training best model...")
 model.fit_(x_pol, yTrain)
 # Calculate loss against test set
@@ -51,30 +48,42 @@ loss = model.loss_(yTest, model.predict_(x_pred))
 print(f"Loss: {loss}")
 
 
-def show_model(model: MyRidge, polynomial: int, title: str) -> None:
+def init_plot(title: str):
     fig, dim_axs = plt.subplots(ncols=3)
     axs = dim_axs.flatten()
-    x_pol = add_polynomial_features(x, polynomial)
-    x_pol = (x_pol - x_pol.mean(axis=0)) / x_pol.std(axis=0)
-    predictions = model.predict_(x_pol)
     for i, feature in enumerate(features):
         # First scatter: other planets -- select X for each features where prediction is 0
-        axs[i].scatter(df[feature], y, color="purple", label="Dataset")
-        # Second scatter: the trained planet -- select X for each features where prediction is 1
-        axs[i].scatter(df[feature], predictions, color="pink", alpha=0.5, label=f"Prediction")
+        axs[i].scatter(df[feature], y, color="purple", s=10, label="Dataset")
         axs[i].set_xlabel(f"{feature}")
         axs[i].set_ylabel(f"Target price")
     plt.title(title)
     plt.legend()
-    plt.show()
+    return axs
+
+
+def fill_plot(axs, model: MyRidge, polynomial: int, color: str) -> None:
+    x_pol = add_polynomial_features(x, polynomial)
+    x_pol = (x_pol - x_pol.mean(axis=0)) / x_pol.std(axis=0)
+    predictions = model.predict_(x_pol)
+    for i, feature in enumerate(features):
+        # Second scatter for each models
+        axs[i].scatter(df[feature], predictions, color=color, alpha=0.5, s=8, label=f"Prediction")
+        axs[i].set_xlabel(f"{feature}")
+        axs[i].set_ylabel(f"Target price")
 
 
 # Show one scatter plot for each features against the price and color = prediction
-show_model(model, int(polynomials), "Best model")
+axs = init_plot("Best model")
+fill_plot(axs, model, int(polynomials), "pink")
+plt.show()
 # -- for each saved models
-for index, thetas in enumerate(other_models):
+cmap = plt.cm.get_cmap('jet', 7)
+for polynomial in range(0, 4):
     # Skip the best model
-    if index + 1 == polynomials:
+    if polynomial + 1 == polynomials:
         continue
-    other_model = MyRidge(thetas)
-    show_model(other_model, index + 1, f"Polynomial {index + 1}")
+    axs = init_plot(f"Polynomial {polynomial + 1}")
+    for index, lambda_ in enumerate(lambdas):
+        other_model = MyRidge(other_models[(polynomial * 6) + index])
+        fill_plot(axs, model, int(polynomials), cmap(index + 1))
+    plt.show()
